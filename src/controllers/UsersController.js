b45734ecc49838cd169375205432d45a18b5ea8e
JoaoLuiss <joaoluisseguro@gmail.com>
2024-01-08 17:26:33 -0300

    Add create method to UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,31 @@
 const AppError = require('../utils/AppError');
+const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
+
+    const database = await sqliteConnection();
+
+    //verify if user email already exists
+    const emailExists = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    );
+    if (emailExists) {
+      throw new AppError('Este email já está em uso');
+    }
+
+    await database.run(
+      'INSERT INTO users ( name, email, password) VALUES (?, ?, ?)',
+      [name, email, password]
+    );
 
     if (!name) {
       throw new AppError('O nome é obrigatório!');
     }
 
-    response.json({ name, email, password });
+    return response.status(201).json();
   }
 }
 
